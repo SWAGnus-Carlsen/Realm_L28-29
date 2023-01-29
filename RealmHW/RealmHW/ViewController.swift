0067e9c476909d24f84fe1b3299440c724a949dd
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2023-01-29 14:13:19 +0300

    Initial commmit

---
+++ RealmHW/RealmHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmHW
+//
+//  Created by Vitaliy Halai on 29.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
